c625a085c1b1fe20113a680ced53e3994e0a0f9d
diogoc <diogomocarvalho11@gmail.com>
2023-03-01 15:49:20 +0000

    create Caixa class

--- src/pt/diogocarvalho/caixa/Main.java
+++ src/pt/diogocarvalho/caixa/Main.java
@@ -3,37 +3,7 @@ package pt.diogocarvalho.caixa;
 public class Main {
 
 	public static void main(String[] args) {
-		printBox(4,4);
-		printBox(8,4);
-		printBox(5,5);
-	}
-	
-	public static void printBox(int width, int height) {
-		if (width < 4) {
-			width = 4;
-		}
-		if (height < 4) {
-			height = 4;
-		}
-		printTopOrBottom(width);
-		printLeftAndRight(width, height - 2);
-		printTopOrBottom(width);
-	}
-
-	public static void printTopOrBottom(int size) {
-		for (int i = 0; i < size; i++) {
-			System.out.print("*");
-		}
-		System.out.println();
-	}	
-	
-	public static void printLeftAndRight(int width, int height) {
-		for (int i = 0; i < height; i++) {
-			System.out.print("*");
-			for (int j = 0; j < width - 2; j++) {
-				System.out.print(" ");
-			}
-			System.out.println("*");
-		}
+		Caixa caixa = new Caixa(4, 4);
+		caixa.print();
 	}
 }
