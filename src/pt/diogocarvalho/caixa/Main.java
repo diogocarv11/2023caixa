2308a8872012834c8c4d03c806f9b06f08b53873
diogoc <diogomocarvalho11@gmail.com>
2023-03-01 14:04:15 +0000

    update gitignore and Main

--- src/pt/diogocarvalho/caixa/Main.java
+++ src/pt/diogocarvalho/caixa/Main.java
@@ -3,8 +3,11 @@ package pt.diogocarvalho.caixa;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("******");
+		System.out.println("*    *");
+		System.out.println("*    *");
+		System.out.println("*    *");
+		System.out.println("******");
 	}
 
 }
